4ea8a73bcdc3700938913f3f89db5af3b5af5657
Marianna26 <44975755+Marianna26@users.noreply.github.com>
2018-11-27 16:54:23 -0600

    Super basic collision check

--- Collision.h
+++ Collision.h
@@ -1,7 +1,7 @@
 #ifndef COLLISION_H_INCLUDED
 #define COLLISION_H_INCLUDED
 
-//checks the four corners of the objects to see if they are touching
+//checks the four corners of Mario and Enemy to see if they are touching
 bool isCollision(Enemy one, Player two)
 {
     if(one.posX < two.posX + two.width && one.posX + one.width  > two.posX && one.posY < two.posY + two.height && 
