dd5ddfdecea77e3f1abc3b356df37df3916a10cb
Marianna26 <44975755+Marianna26@users.noreply.github.com>
2018-11-27 16:51:41 -0600

    Add files via upload

---
+++ Collision.h
@@ -0,0 +1,20 @@
+#ifndef COLLISION_H_INCLUDED
+#define COLLISION_H_INCLUDED
+
+//checks the four corners of the objects to see if they are touching
+bool isCollision(Enemy one, Player two)
+{
+    if(one.posX < two.posX + two.width &&
+       one.posX + one.width  > two.posX &&
+       one.posY < two.posY + two.height &&
+       one.posY + one.height > two.posY)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+#endif // COLLISION_H_INCLUDED
