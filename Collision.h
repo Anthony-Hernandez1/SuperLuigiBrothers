550a0a81ddef8008181d6ed3b70331a165bd3387
Marianna26 <44975755+Marianna26@users.noreply.github.com>
2018-11-27 16:52:53 -0600

    Update Collision.h

--- Collision.h
+++ Collision.h
@@ -4,9 +4,7 @@
 //checks the four corners of the objects to see if they are touching
 bool isCollision(Enemy one, Player two)
 {
-    if(one.posX < two.posX + two.width &&
-       one.posX + one.width  > two.posX &&
-       one.posY < two.posY + two.height &&
+    if(one.posX < two.posX + two.width && one.posX + one.width  > two.posX && one.posY < two.posY + two.height && 
        one.posY + one.height > two.posY)
     {
         return true;
