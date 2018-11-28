870fa12f8a163c2ef372f4e1f7d98637c1720c77
Marianna26 <44975755+Marianna26@users.noreply.github.com>
2018-11-28 14:36:41 -0600

    Update Collision.h

--- Collision.h
+++ Collision.h
@@ -1,18 +1,23 @@
-#ifndef COLLISION_H_INCLUDED
-#define COLLISION_H_INCLUDED
 
+#include <iostream>
+#include "Enemy.h"
+#include "Player.h"
+
+using namespace std;
 //checks the four corners of Mario and Enemy to see if they are touching
+
 bool isCollision(Enemy one, Player two)
 {
-    if(one.posX < two.posX + two.width && one.posX + one.width  > two.posX && one.posY < two.posY + two.height && 
-       one.posY + one.height > two.posY)
+    if(one.getPosX() >= (two.getPosX()) && one.getPosX() <= (two.getPosX() + two.getWidth) ||
+       (one.getPosX() + one.getWidth()) >= two.getPosX() && (one.getPosX() + one.getWidth()) <= two.getPosX() + two.getWidth)
     {
+    
+    if((one.getPosY() >= (two.getPosY()) && one.getPosY() <= (two.getPosY() + two.getHeight) ||
+       (one.getPosY() + one.getHeight()) >= two.getPosY() && (one.getPosY() + one.getHeight()) <= two.getPosY() + two.getHeight))
+        {
         return true;
+        }
     }
-    else
-    {
-        return false;
-    }
+    return false;
 }
 
-#endif // COLLISION_H_INCLUDED
